84eaed8dbfc0364aa686774abbc79d02ccfd8b81
tomasfrancizco-spacedev <tfreire@spacedev.io>
2025-07-29 14:58:47 -0300

    removed create user endpoint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,12 @@
 import {
   Controller,
   Get,
-  Post,
   Body,
   Patch,
   Param,
   Delete,
 } from '@nestjs/common';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 /**
@@ -17,11 +15,6 @@ import { UpdateUserDto } from './dto/update-user.dto';
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
-  
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.createUser(createUserDto);
-  }
 
   @Get()
   findAll() {
